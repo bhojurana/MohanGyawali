f84e55d4d420abdab21b102b28016ee0ac7ca0a2
bhojurana <42022215+bhojurana@users.noreply.github.com>
2022-08-16 15:02:25 +0545

    blank

--- wp-content/themes/divergent/searchform.php
+++
@@ -1,4 +0,0 @@
-	<form method="get" class="searchbox" id="searchform" action="<?php esc_url( home_url( '/' ) ); ?>/">
-		<input type="text" class="searchtext" name="s" id="s" />
-        <button type="submit" class="cv-button"><i class="fas fa-search"></i></button>
-	</form>
